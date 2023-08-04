8c1efffe4995f5bcbaf7fa303aadb2f1a799e26f
ilias bissoufa <bissoufailias@gmail.com>
2023-08-04 17:12:31 +0100

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,31 @@
+
+import Weather from '../assets/projects/weather-app.jpeg'
+import BrandApp from '../assets/projects/brandapp.png'
+import CrudApp from '../assets/projects/crud-app.jpeg'
+
+export const data=[
+    {
+        id:1,
+        name:"Weather App",
+        image:Weather,
+        github:"",
+        live:"",
+    },
+    {
+        id:2,
+        name:"Brand App",
+        image:BrandApp,
+        github:"",
+        live:"",
+    },
+    {
+        id:3,
+        name:"Crud App",
+        image:CrudApp,
+        github:"",
+        live:"",
+    },
+
+
+
+]
